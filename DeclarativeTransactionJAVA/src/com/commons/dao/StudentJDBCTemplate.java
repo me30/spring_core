956574855b7bcb89caf0b495deb391ff92ce503a
minesh <minesh,b.java>
2021-03-11 09:51:05 +0530

    declarative and programatically code transaction

---
+++ DeclarativeTransactionJAVA/src/com/commons/dao/StudentJDBCTemplate.java
@@ -0,0 +1,46 @@
+package com.commons.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.commons.entity.StudentMarks;
+
+public class StudentJDBCTemplate implements StudentDAO {
+   private JdbcTemplate jdbcTemplateObject;
+
+   public void setDataSource(DataSource dataSource) {
+      this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+   }
+   public void create(String name, Integer age, Integer marks, Integer year){
+      try {
+         String SQL1 = "insert into Student (name, age) values (?, ?)";
+         jdbcTemplateObject.update( SQL1, name, age);
+
+         // Get the latest student id to be used in Marks table
+         String SQL2 = "select max(id) from Student";
+         Number number = jdbcTemplateObject.queryForObject(SQL2, null, Integer.class);
+
+         String SQL3 = "insert into Marks(sid, marks, year) " + "values (?, ?, ?)";
+         jdbcTemplateObject.update( SQL3, number.intValue(), marks, year);
+         System.out.println("Created Name = " + name + ", Age = " + age);
+         
+         // to simulate the exception.
+         //throw new RuntimeException("simulate Error condition") ;
+      } 
+      catch (DataAccessException e) {
+         System.out.println("Error in creating record, rolling back");
+         throw e;
+      }
+   }
+   public List<StudentMarks> listStudents() {
+      String SQL = "select * from Student, Marks where Student.id = Marks.sid";
+      List <StudentMarks> studentMarks = jdbcTemplateObject.query(SQL, 
+         new StudentMarksMapper());
+      
+      return studentMarks;
+   }
+}
