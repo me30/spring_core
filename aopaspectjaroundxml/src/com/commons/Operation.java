ad0b4aa4030c8c4bef0f32499dacb5cdf480ac49
minesh <minesh,b.java>
2021-03-11 21:17:24 +0530

    code for aop

---
+++ aopaspectjaroundxml/src/com/commons/Operation.java
@@ -0,0 +1,5 @@
+package com.commons;
+public  class Operation{
+	public void msg(){System.out.println("msg() is invoked");}
+	public void display(){System.out.println("display() is invoked");}
+}
---
+++ aopaspectjaroundxml/src/com/commons/TrackOperation.java
@@ -0,0 +1,12 @@
+package com.commons;
+import org.aspectj.lang.ProceedingJoinPoint;
+public class TrackOperation
+{
+	public Object myadvice(ProceedingJoinPoint pjp) throws Throwable 
+	{
+		System.out.println("Additional Concern Before calling actual method");
+		Object obj=pjp.proceed();
+		System.out.println("Additional Concern After calling actual method");
+		return obj;
+	}
+}
